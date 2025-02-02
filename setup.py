bcbd743a8f9210eee36df9acc193399a93462a33
SalilHarit <salilharit2001@gmail.com>
2025-02-02 21:11:23 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+  '''
+    Read requirements file and return list of requirements
+  
+  '''
+  requirements=[]
+  with open(file_path) as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='mlproject',
+    version='0.1',
+    author='Salil Harit',
+    author_email='salilharit2001@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+)
+
